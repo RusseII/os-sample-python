f7808250bdbbac3337d0038a84cb9554d704bf53
Russell Ratcliffe <rratcliffe57@gmail.com>
2016-09-18 23:47:01 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hef34232dsfld!"
-    
-@app.route("/hi")
-def hello23():
-    return "Hewef34232dsfld!"
+
 
 if __name__ == "__main__":
     app.run()
